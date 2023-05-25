182a5ddf06c05caafc5fc1c089b0a75b5363cdb2
Владислав <vladislav-marinkin@outlook.com>
2023-05-25 23:39:08 +0400

    Largest prime factor

--- Largest prime factor/Program.cs
+++ Largest prime factor/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            long num = 600851475143;
+            long largestPrimeFactor = FindLargestPrimeFactor(num);
+
+            Console.WriteLine(largestPrimeFactor);
+        }
+
+        static long FindLargestPrimeFactor(long number)
+        {
+            long largestFactor = 0;
+
+            while (number % 2 == 0)
+            {
+                largestFactor = 2;
+                number /= 2;
+            }
+
+            for (long i = 3; i <= Math.Sqrt(number); i += 2)
+            {
+                while (number % i == 0)
+                {
+                    largestFactor = i;
+                    number /= i;
+                }
+            }
+
+            if (number > 1)
+            {
+                largestFactor = number;
+            }
+
+            return largestFactor;
         }
     }
 }
