75de00b8c69f83c0d762e2d65d638f21bc33e9c7
Владислав <vladislav-marinkin@outlook.com>
2023-05-25 23:14:08 +0400

    Smallest multiple

---
+++ Smallest multiple/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Smallest_multiple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long num = 2520;
+            bool flag = true;
+
+            while (flag)
+            {
+                num += 2520;
+
+                for (int i = 11; i <= 20; i++)
+                {
+                    if (num % i != 0)
+                    {
+                        flag = true;
+                        break;
+                    }
+                    flag = false;
+                }
+            }
+
+            Console.WriteLine(num);
+        }
+    }
+}
